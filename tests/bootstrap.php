5a8e1475300e68eed42a93de51798a002c2f0cd0
gin0115 <glynn.quelch@gmail.com>
2021-01-20 02:32:21 +0000

    House Keeping

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,13 +1,5 @@
 <?php
 
-use PinkCrab\HTTP\HTTP;
-use GuzzleHttp\Psr7\Response;
-use PinkCrab\Registerables\Ajax;
-use GuzzleHttp\Psr7\ServerRequest;
-use PinkCrab\Core\Application\App;
-use PinkCrab\Core\Services\Dice\WP_Dice;
-use PinkCrab\Core\Services\Dice\Dice; //@TODO!!!
-use PinkCrab\Core\Services\ServiceContainer\Container;
 
 /**
  * PHPUnit bootstrap file
@@ -22,18 +14,7 @@ require_once getenv( 'WP_PHPUNIT__DIR' ) . '/includes/functions.php';
 tests_add_filter(
 	'muplugins_loaded',
 	function() {
-		$app = App::init( new Container() );
-		$di  = WP_Dice::constructWith( new Dice() );
-		$di->addRules(
-			array(
-				Ajax::class => array(
-					'constructParams' => array( ( new HTTP() )->request_from_globals() ),
-					'shared'          => true,
-					'inherit'         => true,
-				),
-			)
-		);
-		$app->set( 'di', $di );
+
 	}
 );
 
